f1c94b5061eedd575cda07592fe5f238195a4d0f
Prabhat <mmrprabhujii@gmail.com>
2024-07-10 22:19:09 +0530

    first adding file

---
+++ src/components/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <>
+    <div id='up-main'>
+        <h1 id='h-1'>Let us find your</h1>
+        <h1 id='h-2'>forever Food.</h1>
+        <p id='p-1'>Lorem ipsum dolor sit amet consectetur, adipisicing elt.</p>
+        <p id='p-2'>Nesciunt illo tenetur fuga lucimus numquam ea!</p>
+        <button id='btn-1'>Search now</button>
+        <button id='btn-2'>Konw more</button>
+    </div>
+    <div id='mid-main'>
+        <div>
+            <img id='img' src="https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80" alt="" />
+        </div>
+        <div id='mid-pra'>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, debitis.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, molestiae! Quidem est esse numquam odio deleniti, beatae, magni dolores provident quaerat totam eos, aperiam architecto eius quis quibusdam fugiat dicta.</p>
+            <button>Get in Touch</button>
+        </div>
+    </div>
+    </>
+  )
+}
